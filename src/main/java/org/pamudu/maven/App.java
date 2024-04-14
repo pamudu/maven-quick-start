4cc12d42cb400276de2ae575355bcfb4bddc8617
pamudu <pamuduprabathiya28@gmail.com>
2024-04-14 13:02:02 +0530

    generated simple maven project

---
+++ src/main/java/org/pamudu/maven/App.java
@@ -0,0 +1,13 @@
+package org.pamudu.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
